b3ce315f27a8fc680b319dbbdc3ffa99cffc1823
kovalenkos <kovalenkova1983@gmail.com>
2018-08-29 23:34:30 +0300

    Добавьте файлы проекта.

---
+++ Login/App_Start/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Login.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+[assembly: OwinStartup(typeof(Login.Startup))]
+
+namespace Login
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // настраиваем контекст и менеджер
+            app.CreatePerOwinContext<ApplicationContext>(ApplicationContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
